0fa06a80f1d5229ca75834c231346a54f9536e39
taoeer <719012229@qq.com>
2022-01-25 12:05:40 +0800

    ok

---
+++ README.md
@@ -0,0 +1,5 @@
+# add 'Generate React component' to the explore tree menu
+
+generate files: 
+ ${name}/index.js
+ ${name}/index.module.less
---
+++ src/generateReactComponent.ts
@@ -0,0 +1,36 @@
+import { Uri, window, workspace } from "vscode";
+
+const jsFileTemplate = (name: any) =>
+  `
+import React from 'react';
+import css from 'index.module.less';
+
+const ${name} = () => {
+  return null;
+}
+
+export default ${name};`.trim();
+
+export default async (uri?: Uri) => {
+  if (!uri) {
+    return window.showErrorMessage("No file path found.");
+  }
+
+  const componentName = await window.showInputBox({
+    prompt: "Component name",
+  });
+
+  if (!componentName) {
+    return window.showErrorMessage("No component name passed");
+  }
+
+  workspace.fs.writeFile(
+    Uri.file(`${uri.path}/${componentName}/index.js`),
+    new Uint8Array(Buffer.from(jsFileTemplate(componentName)))
+  );
+
+  workspace.fs.writeFile(
+    Uri.file(`${uri.path}/${componentName}/index.module.less`),
+    new Uint8Array(Buffer.from(""))
+  );
+};
